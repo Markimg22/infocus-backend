8b9c7b5561620426b865eed04200c70526497bf2
Markimg22 <marquim.net.221200@hotmail.com>
2021-04-06 18:45:53 -0400

    teste rota 3

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-	return res.render(`
-    <h1>Hello world</h1>
-  `);
+	return res.json({ message: 'Hello world' });
 });
 
 export { router };
