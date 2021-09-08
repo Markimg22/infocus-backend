215cb179f3e5234a243ed4def9d5977acab81c56
Markimg22 <marquim.net.221200@hotmail.com>
2021-09-08 01:46:12 -0400

    default connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,8 @@ import { createConnection } from 'typeorm';
 
 require('dotenv').config();
 
-createConnection(process.env.NODE_ENV).then(() => {
+createConnection(process.env.NODE_ENV).then((_connection) => {
+  console.log('Connection', _connection.driver);
   console.log('📦 Connected to database.');
 }).catch((error) => {
   console.error(`Error connecting to database: ${error}.`);
