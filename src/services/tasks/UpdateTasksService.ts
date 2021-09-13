30572b3d8e139c030e0b02cd02f96165f7f10f90
Markimg22 <marquim.net.221200@hotmail.com>
2021-09-13 12:28:59 -0400

    correction update total_tasks_completed

--- src/services/tasks/UpdateTasksService.ts
+++ src/services/tasks/UpdateTasksService.ts
@@ -3,18 +3,19 @@ import { getConnection } from 'typeorm';
 import { TasksRepositories } from '../../repositories/TasksRepositories';
 
 import { ListTasksService } from './ListTasksService';
+import { UpdatePerformanceService } from '../performances/UpdatePerformanceService';
 
 interface ITasksRequest {
-  task_id: string;
   user_id: string;
+  task_id: string;
 }
 
 class UpdateTasksService {
-  async execute({ task_id, user_id }: ITasksRequest) {
+  async execute({ user_id, task_id }: ITasksRequest) {
     const tasksRepositories = getConnection(process.env.NODE_ENV).getCustomRepository(TasksRepositories);
 
     // Task exists
-    const task = await tasksRepositories.findOne({ user_id, id: task_id });
+    let task = await tasksRepositories.findOne({ user_id, id: task_id });
 
     if (!task) {
       throw new Error('Task not exists.');
@@ -22,6 +23,16 @@ class UpdateTasksService {
 
     // Update task
     await tasksRepositories.update({ id: task.id }, { is_completed: !task.is_completed });
+
+    // Update performance
+    const updatePerformanceService = new UpdatePerformanceService();
+    task = await tasksRepositories.findOne({ user_id, id: task_id });
+
+    await updatePerformanceService.execute({
+      user_id,
+      value: task.is_completed ? 1 : -1,
+      data_to_update: 'total_tasks_completed',
+    });
 
     // List tasks
     const listTasksService = new ListTasksService();
